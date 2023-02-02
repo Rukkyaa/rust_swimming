46abad083970cdf2767bf9c7984c1e4f0ef3d3a2
Axel LAMBERT <axlamber@e2r13p8.clusters.42paris.fr>
2023-02-02 18:05:17 +0100

    Ca avance bien

--- R00/ex05/target/debug/deps/ex05-0281f01fcc2d580b.d
+++
@@ -1,5 +0,0 @@
-/mnt/nfs/homes/axlamber/Documents/piscine_rust/R00/ex05/target/debug/deps/ex05-0281f01fcc2d580b: src/main.rs
-
-/mnt/nfs/homes/axlamber/Documents/piscine_rust/R00/ex05/target/debug/deps/ex05-0281f01fcc2d580b.d: src/main.rs
-
-src/main.rs:
--- R00/ex05/target/debug/ex05.d
+++
@@ -1,1 +0,0 @@
-/mnt/nfs/homes/axlamber/Documents/piscine_rust/R00/ex05/target/debug/ex05: /mnt/nfs/homes/axlamber/Documents/piscine_rust/R00/ex05/src/main.rs
