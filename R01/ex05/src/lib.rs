c24a3cc4ea64211028a2304e0b452d9bd1afd2f5
Axel LAMBERT <axlamber@e2r13p8.clusters.42paris.fr>
2023-02-06 10:16:17 +0100

    Remove main function + rename the file

--- R01/ex05/src/lib.rs
+++ R01/ex05/src/lib.rs
@@ -3,31 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   main.rs                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rukkyaa <rukkyaa@student.42.fr>            +#+  +:+       +#+        */
+/*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 09:53:00 by rukkyaa           #+#    #+#             */
-/*   Updated: 2023/02/05 16:49:38 by rukkyaa          ###   ########.fr       */
+/*   Updated: 2023/02/06 10:13:38 by axlamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 fn deduplicate(list: &mut Vec<i32>) {
-    let mut i = 0;
-    while i < list.len() {
-        let mut j = i + 1;
-        while j < list.len() {
-            if list[j] == list[i] {
-                list.remove(j);
-            } else {
-                j += 1;
-            }
-        }
-        i += 1;
-    }
-}
+	let mut	i = 0;
+	let mut j;
 
-fn main() {
-	let mut v = vec![1, 2, 2, 3, 2, 4, 3, 2, 2];
-	deduplicate(&mut v);
-	assert_eq!(v, [1, 2, 3, 4]);
-    // println!("Hello, world!");
+	while i < list.len() {
+		j = i + 1;
+		while j < list.len() {
+			if list[j] == list[i] {list.remove(j);}
+			else {j += 1}
+        }
+        i += 1
+    }
 }
